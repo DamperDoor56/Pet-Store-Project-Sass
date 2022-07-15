827fb7dd3336f4aa2517e22da3acd61ca13f0291
Ludmi Lopez <ludmit@outlook.com>
2022-07-14 21:23:01 -0300

    some-style

--- js/index.js
+++ js/index.js
@@ -15,9 +15,11 @@ document.querySelector('#menu-btn').onclick = () =>{
 
 //Navbar transparent & colored on scroll
 window.onscroll = () => {
-    if(window.scrollY > 0){
+    if(window.scrollY > 300){
         document.querySelector('.header').classList.add('active');
+        document.querySelector('.btn').classList.add('active');
     } else {
         document.querySelector('.header').classList.remove('active');
+        document.querySelector('.btn').classList.remove('active');
     }
 }
